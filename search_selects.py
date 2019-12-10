336c263f54fc5bb618d7fef0f545e65f82dfb732
Alberto García Sola <44441820+dawalberto@users.noreply.github.com>
2019-12-10 01:56:00 +0100

    first commit

---
+++ search_selects.py
@@ -0,0 +1,60 @@
+import os
+
+l_all = []
+found_select = False
+current_select = ''
+
+dirName = os.getcwd()
+print('Current dir: ' + dirName)
+
+def print_select(select, file, numLine):
+  all_selects.write('################################################################################################## \r\n')
+  all_selects.write('\r\n')
+  all_selects.write('ARCHIVO: ' + file + '\r\n')
+  all_selects.write('LINEA: ' + str(numLine) + '\r\n')
+  all_selects.write(select + '\r\n')
+  all_selects.write('################################################################################################## \r\n')
+
+def getListOfFiles(dirName):
+  # create a list of file and sub directories 
+  # names in the given directory 
+  listOfFile = os.listdir(dirName)
+  allFiles = list()
+  # Iterate over all the entries
+  for entry in listOfFile:
+    # Create full path
+    fullPath = os.path.join(dirName, entry)
+    # If entry is a directory then get the list of files in this directory 
+    if os.path.isdir(fullPath):
+      allFiles = allFiles + getListOfFiles(fullPath)
+    else:
+      allFiles.append(fullPath)
+              
+  return allFiles  
+
+# Get the list of all files in directory tree at given path
+listOfFiles = getListOfFiles(dirName)
+
+# Print the files
+all_selects = open('all_selects.txt', 'w+')
+
+for file in listOfFiles:
+  print(file)
+  current_file = file
+  with open(file, 'r') as f:
+    for num, line in enumerate(f, 1):
+      if (line.rfind('= " select') > -1 or line.rfind('= "select') > -1 or line.rfind('=" select') > -1 or line.rfind('= " Select') > -1 or line.rfind('= "Select') > -1 or line.rfind('=" Select') > -1 or line.rfind('= " SELECT') > -1 or line.rfind('= "SELECT') > -1 or line.rfind('=" SELECT') > -1) and file.rfind('search_selects.py') == -1:
+        found_select = True
+        line_select = num
+      if found_select:
+        current_select += line
+        if line.rfind(';') > -1:
+          found_select = False
+          l_all.append(current_select)
+          print_select(current_select, current_file, line_select)
+          current_select = ''
+
+all_selects.write('\r\n')
+all_selects.write('----------------------------------\r\n')
+all_selects.write('TOTAL SELECTS: ' + str(len(l_all)))
+all_selects.close()
